41b4e8ca493b00a209d72875c9a0816849e64c61
Felipe DR <felipe.corp7@gmail.com>
2025-08-03 17:56:03 -0300

    build: class-01 creating global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Inter } from 'next/font/google';
 
 import StyledComponentsRegistry from '@/lib/registry';
 
+import { Providers } from './providers';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -18,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <StyledComponentsRegistry>
+          <Providers>{children}</Providers>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
