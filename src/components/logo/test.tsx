6c02a2008fabb680b84073428258b1ba598c00fc
Felipe DR <felipe.corp7@gmail.com>
2025-08-31 14:55:59 -0300

    feat: class-08 creating a renderWithTheme test helper

--- src/components/logo/test.tsx
+++ src/components/logo/test.tsx
@@ -1,9 +1,3 @@
-// import Logo from '.';
-
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    // const { container } = render(<Logo />);
-    // expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-    // expect(container.firstChild).toMatchSnapshot()
-  });
+  it('should render a white label by default', () => {});
 });
