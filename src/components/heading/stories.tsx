fe38e8ad76a9ed11ec587ea0afc15e67a4a65dbc
Felipe DR <felipe.corp7@gmail.com>
2025-09-06 14:51:12 -0300

    feat: class-09 creating heading component and variants

--- src/components/heading/stories.tsx
+++ src/components/heading/stories.tsx
@@ -2,7 +2,7 @@ import { Meta } from '@storybook/react';
 
 import Heading, { HeadingProps } from '.';
 
-export default { component: Heading } as Meta;
+export default { title: 'Heading', component: Heading } as Meta;
 
 export const Default = {
   render: (args: HeadingProps) => <Heading {...args} />,
