3bd81442747b955e900dafe8ce02335e36547932
Felipe DR <felipe.corp7@gmail.com>
2025-09-13 13:22:03 -0300

    feat: class-10 creating a mobile version from menu

---
+++ src/components/menu/stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Menu from '.';
+
+const meta: Meta = {
+  title: 'Menu',
+  component: Menu,
+  argTypes: {},
+};
+
+export default meta;
+
+export const Default: StoryObj = {};
