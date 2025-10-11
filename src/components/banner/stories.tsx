1aae3e1e56e5ccb3d4a392f6920397d0c67a5d84
Felipe DR <felipe.corp7@gmail.com>
2025-10-11 13:04:36 -0300

    feat: class-12 banner more ribbon - solution

--- src/components/banner/stories.tsx
+++ src/components/banner/stories.tsx
@@ -1,10 +1,24 @@
 import { Meta, StoryObj } from '@storybook/react';
 
 import Banner, { BannerProps } from '.';
+import { RibbonProps } from '../ribbon';
 
 export default {
   title: 'Banner',
   component: Banner,
+  argTypes: {
+    ribbon: {
+      type: 'string',
+    },
+    ribbonColor: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary'],
+    },
+    ribbonSize: {
+      control: { type: 'select' },
+      options: ['small', 'normal'],
+    },
+  },
   args: {
     img: 'https://images.unsplash.com/photo-1745750747043-da33e463f361?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1170',
     title: 'Defy death',
@@ -23,4 +37,18 @@ export const Default: StoryObj<BannerProps> = {
       <Banner {...args} />
     </div>
   ),
+};
+
+export const WithRibbon: StoryObj<BannerProps & RibbonProps> = (
+  args: BannerProps & RibbonProps,
+) => (
+  <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+    <Banner {...args} />
+  </div>
+);
+
+WithRibbon.args = {
+  ribbon: '20% OFF',
+  ribbonSize: 'normal',
+  ribbonColor: 'primary',
 };
